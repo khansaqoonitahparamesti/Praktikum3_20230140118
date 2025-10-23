eb05e8b9fca9591dabfe27f49b955d5230b9e306
khansaqoonitahparamesti <161275533+khansaqoonitahparamesti@users.noreply.github.com>
2025-10-23 08:58:48 +0700

    Add files via upload

---
+++ pertemuan3/middleware/permissionMiddleware.js
@@ -0,0 +1,20 @@
+ 	exports.addUserData = (req, res, next) => {
+ 	  console.log('Middleware: Menambahkan data user dummy...');
+ 	  req.user = {
+ 	    id: 123,
+ 	    nama: 'User Karyawan',
+ 	    role: 'karyawan'
+ 	  };
+ 	  next(); 
+ 	};
+ 	
+ 	exports.isAdmin = (req, res, next) => {
+ 	  if (req.user && req.user.role === 'admin') {
+ 	    console.log('Middleware: Izin admin diberikan.');
+ 	    next(); 
+ 	  } else {
+ 	    console.log('Middleware: Gagal! Pengguna bukan admin.');
+ 	    return res.status(403).json({ message: 'Akses ditolak: Hanya untuk admin'});
+ 	  }
+ 	};
+ 	
